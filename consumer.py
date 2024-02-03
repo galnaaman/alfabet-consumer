51771687ac8a5d7224e30ab4fb84376a9e40804a
galnaaman <85794518+galnaaman@users.noreply.github.com>
2024-02-04 00:19:19 +0200

    first commit

---
+++ consumer.py
@@ -0,0 +1,33 @@
+import pika
+import json
+import os
+
+RABBITMQ_HOST = 'localhost'
+RABBITMQ_QUEUE = 'event_notifications'
+
+
+def callback(ch, method, properties, body):
+    print(f"Received {body}")
+    data = json.loads(body)
+    for participant in data['participants']:
+        print(f"Sending reminder to {participant}")
+        # ----------------- Send email to participant -----------------
+        # ----------------- use email service logic -----------------
+
+    print(f"Sent reminder for event '{data['event_name']}' to {len(data['participants'])} participants.")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+    channel = connection.channel()
+
+    channel.queue_declare(queue=RABBITMQ_QUEUE, durable=True)
+    channel.basic_consume(queue=RABBITMQ_QUEUE, on_message_callback=callback, auto_ack=False)
+
+    print("Starting consumer. To exit press CTRL+C")
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    main()
